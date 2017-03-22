3529b70202e01aa72386aa22f5e183f160ba1f08
Anton Bukov <k06aaa@gmail.com>
2017-03-22 22:29:52 +0300

    Ensure tests runs sequentially

--- Example/Tests/AbstractTests.m
+++ Example/Tests/AbstractTests.m
@@ -12,12 +12,27 @@
 
 @implementation AbstractTests
 
+static dispatch_group_t runTestsSequentially;
+
+- (void)setUp {
+    [super setUp];
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        runTestsSequentially = dispatch_group_create();
+    });
+    
+    dispatch_group_enter(runTestsSequentially);
+}
+
 - (void)tearDown {
     [super tearDown];
     
     XCTAssertTrue([DeluxeInjection injectedClasses].count == 0,
                   @"Some injections still exists: %@",
                   [DeluxeInjection debugDescription]);
+    
+    dispatch_group_leave(runTestsSequentially);
 }
 
 @end
